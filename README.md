381d4746ed80fbcb3606978e31c5ca20fb2e699a
Benjamin Féron <benjamin.feron@gmail.com>
2022-01-23 22:08:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# goalkicker-looter
+
+A bash script that help to easily download books from https://goalkicker.com
+
+## Repository
+
+[https://github.com/benjamin-feron/goalkicker-looter](https://github.com/benjamin-feron/goalkicker-looter)
+
+## Usage
+```bash
+$ git clone https://github.com/benjamin-feron/goalkicker-looter.git
+$ ./goalkicker-looter.sh
+```
+
+## Options
+```
+    -l, --list           List available books.
+    -n, --name           Book name. To list available books, une -n or --name option.
+    -f, --force          Force downloading of already downloaded books.
+    --help               Show help
+```
---
+++ goalkicker-looter.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+###########################
+# goalkicker-looter 1.0.0 #
+###########################
+
+show_help () {
+  echo "Usage: goalkicker-looter [OPTIONS]
+  Options:
+    -l, --list           List available books.
+    -n, --name           Book name. To list available books, une -n or --name option.
+    -f, --force          Force downloading of already downloaded books.
+    --help               Show help"
+}
+
+URL=https://goalkicker.com
+DEST=./books
+
+LIST=0
+NAME=""
+FORCE=0
+
+while :; do
+  case $1 in
+    -l|--list)
+      LIST=1
+      shift
+      ;;
+    -n|--name)
+      NAME=${2}
+      shift
+      ;;
+    -f|--force)
+      FORCE=1
+      shift
+      ;;
+    --help)
+      show_help
+      exit
+      ;;
+    --)
+      shift
+      break
+      ;;
+    -?*)
+      printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+      ;;
+    *)
+      break
+  esac
+  shift
+done
+
+# Obtain books list
+books=$(curl -s $URL | grep -Eo 'href="[^"]*Book[0-9]*' | grep -Ev https\? | cut -c 7-)
+
+# List books
+if [ $LIST == 1 ]; then
+  for book in $books; do
+    [[ "$NAME" != "" ]] && [[ ! $book =~ "${NAME}Book" ]] && continue
+    echo "$book" | sed 's/\(.\+\)Book/\1/'
+  done
+  exit
+fi
+
+# Make destination directory
+[ -d "$DEST" ] || mkdir "$DEST"
+
+# Download books
+for book in $books; do
+  [[ ! $book =~ "${NAME}Book" ]] && echo "Book not found, une -l or --list option to list available books" && exit 1
+  book_file_name=`curl -s "$URL/$book/" | grep -e '.*<button.\+class="download".\+[a-z]<\/button>' | sed "s/.\+onclick=\"location\.href='\(.\+\)'.\+/\1/"`
+  book_dest="$DEST/$book_file_name"
+  echo "Downloading $URL/$book_page_url/$book_file_name..."
+  if [[ $FORCE == 0 ]]; then
+    [ -f "$book_dest" ] && echo "File already exists, use -f option to force downloading." && [[ "$NAME" != "" ]] && break
+  fi
+  curl -s "$URL/$book_page_url/$book_file_name" -o "$book_dest"
+  echo "Done."
+  [[ "$NAME" != "" ]] && break
+done
+
